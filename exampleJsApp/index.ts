b3f66b1ca5a79c1fbd23f1e132a4cf5587209754
Tom Windels <tom.windels@outlook.com>
2023-05-17 16:31:19 +0200

    Configured gradle to run test
    Added gradle task to run example JS app after building
    Reverted test to use the Solid Playground pod

--- exampleJsApp/index.ts
+++ exampleJsApp/index.ts
@@ -1,17 +1,18 @@
+import { Triple } from "n3";
 import { SolidConnection, Logging } from "ldests";
 
 async function main() {
     console.log("Creating a connection");
     const connection = new SolidConnection.Builder()
-        .url("http://localhost:3000/")
-        .root("sensor_data/946715229402/")
+        .url("https://pod.playground.solidlab.be/")
         .create();
     console.log("Fetching directories...");
     const directories = await connection.root.directories();
     console.log(`${directories.length} directories found!`);
-    console.log("In triples:");
-    const triples = await connection.root.data();
-    triples.forEach(triple => console.log(triple));
+    // not necessary now
+    // console.log("In triples:");
+    // const triples = await connection.root.data();
+    // triples.forEach(triple => console.log((triple as Triple).toJSON()));
     console.log("Closing the connection...");
     await connection.close();
     // if the length is 0, the playground pod is either empty (unlikely), or something
--- jsLibrary/build.gradle.kts
+++ jsLibrary/build.gradle.kts
@@ -16,14 +16,21 @@ plugins {
 //    project.tasks.getByName("jsNodeRun").dependsOn(project.tasks.getByName("jsProductionLibraryCompileSync"))
 //}
 
-// task to copy the resulting generated code to the example app's modules
 afterEvaluate {
+    // task to copy the resulting generated code to the example app's modules
     val updateExampleApp = tasks.create("updateExampleApp", Copy::class.java) {
         from("../build/js/packages")
         into("../exampleJsApp/node_modules")
     }
+    // task to test the library by running the `exampleJsApp`
+    val testLibraryTask = tasks.create("testLibraryTask", Exec::class.java) {
+        commandLine = listOf("npx", "ts-node", "../exampleJsApp/index.ts")
+    }
     project.tasks.getByName("build").finalizedBy(updateExampleApp)
     updateExampleApp.dependsOn(project.tasks.getByName("build"))
+
+    // configuring the test to run as well
+    updateExampleApp.finalizedBy(testLibraryTask)
 }
 
 kotlin {
